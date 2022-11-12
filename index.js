4fb8864c771d3f6994a00efd14019e0995210eaa
Jose Moya <jose.moya1901@alumnos.ubiobio.cl>
2022-11-12 16:02:58 -0300

    bd

--- index.js
+++ index.js
@@ -4,16 +4,6 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const productRoutes = require('./routes/productRoutes');
-const categoryRoutes = require('./routes/categoryRoutes')
-
-app.use(cors())
-app.use(express.json());
-app.options('*', cors());
-app.use('/api', productRoutes);
-app.use('/api', categoryRoutes);
-
-
 
 app.listen(process.env.PORT, () => {
     console.log('El proyecto esta corriendo en el puerto -> ', process.env.PORT)
