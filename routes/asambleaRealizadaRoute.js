6f983cdb7d939daeca7dba140cc728cc697169a3
Jose Moya <jose.moya1901@alumnos.ubiobio.cl>
2022-11-13 19:37:03 -0300

    arreglo bug

--- routes/asambleaRealizadaRoute.js
+++ routes/asambleaRealizadaRoute.js
@@ -7,3 +7,5 @@ api.get('/asambleaRealizada', asambleaRealizadaController.getAsambleasRealizadas
 api.get('/asambleaRealizada/search/:id', asambleaRealizadaController.getSpecificAsambleaRealizada);
 api.put('/asambleaRealizada/update/:id', asambleaRealizadaController.updateAsambleaRealizada);
 api.get('/asambleaRealizada/delete/:id', asambleaRealizadaController.deleteAsambleaRealizada);
+
+module.exports = api;
